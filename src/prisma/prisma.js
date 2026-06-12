b4f5b40252d6b6cc94527593b28283ea9adc17c3
Andrea Crespillo <andrea.crespillo@est.fi.uncoma.edu.ar>
2026-06-11 22:28:26 -0300

    - pg

--- src/prisma/prisma.js
+++ src/prisma/prisma.js
@@ -1,16 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
-const { neonConfig, Pool } = require('@neondatabase/serverless');
-const ws = require('ws');
-
-// Esto es necesario para que el pooler de Neon funcione correctamente en entornos Node serverless
-neonConfig.webSocketConstructor = ws;
+const { Pool } = require('pg'); 
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be set to connect to the database');
 }
 
-// Usamos el Pool optimizado de Neon
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
