25e0b53f79e2c10138f6adafad2c5e9ac911549c
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-06-13 00:08:46 -0300

    - getAll, getUserProfile, createUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,31 @@
-const getAll = async (req, res) => { }
-const getProfile = async (req, res) => { }// Pasa a auth como register cuando se implemente autenticación
+const getAll = async (req, res) => {
+    try {
+        const users = await getAllUsersService();
+        return res.status(200).json({
+            success: true,
+            data: users,
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+const getProfile = async (req, res) => {
+    try {
+        const user = await getUserProfileService(req.user);
+        if (!user) {
+            res.status(404).json({ error: "Not found" });
+            return;
+        }
+        return res.status(200).json({
+            success: true,
+            data: user,
+
+
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}// Pasa a auth como register cuando se implemente autenticación
 const updateProfile = async (req, res) => { }
 const deleteUser = async (req, res) => { }
 const createUser = async (req, res) => { }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,37 @@
 const prisma = require('../prisma/prisma');
-const getAllUsersService = async () => {}
-const getUserProfileService = async (userId) => { }
-const updateUserProfileService = async (userId, profileData) => { }
-const deleteUserService = async (userId) => { }
-const createUserService = async (userData) => { }
+
+async function getAllUsersService() {
+    return await prisma.user.findMany();
+}
+async function getUserProfileService(user) {
+    return await prisma.user.findUnique({
+        where: { email: user.email, password: user.password }
+    });
+}
+async function updateUserProfileService(userId, profileData) { }
+async function deleteUserService(userId) { }
+
+async function createUserService(userData) {
+
+    const { email, password, role } = userData;
+    const existing = await prisma.user.findUnique({ where: { email } });
+    if (existing) {
+        throw new Error("El email ya está registrado");
+    }
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            role: role || "USUARIO",
+        },
+    });  
+
+    return {
+        user: { id: user.id, email: user.email, role: user.role }
+    };
+}
+
 
 module.exports = {
     getAllUsersService,
