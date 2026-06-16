2a3920915a730af1c856cea3dd15d153eb825182
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-06-15 23:47:16 -0300

    - Install ando configure cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const boardgamesRoutes = require('./routes/boardgames.routes');
 const favoritesRoutes = require('./routes/favorites.routes');
@@ -8,9 +9,9 @@ const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-
 app.use('/api', healthRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/favorites', favoritesRoutes);
