ca4a552aace35c49fd71afe3c58530b96f8521f7
Andrea Crespillo <andrea.crespillo@est.fi.uncoma.edu.ar>
2026-06-24 10:30:08 -0300

    - Add authenticate and authorizeAdmin in routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const { getAll, createUser, getProfile, updateProfile, deleteUser } = require('../controllers/user.controller');
 const { validateIdParam } = require('../validations/user.validation');
-const { authenticate} = require('../middlewares/auth');
+const { authenticate, authorizeAdmin} = require('../middlewares/auth');
 
 const router = Router();
 
 router.get("/", authenticate, getAll);
 router.get("/profile", authenticate, getProfile);
-router.put("/profile/:id", authenticate, validateIdParam, updateProfile);
-router.delete("/", authenticate, deleteUser);
+router.put("/profile/:id", authenticate, authorizeAdmin, validateIdParam, updateProfile);
+router.delete("/", authenticate, authorizeAdmin, deleteUser);
 module.exports = router;
